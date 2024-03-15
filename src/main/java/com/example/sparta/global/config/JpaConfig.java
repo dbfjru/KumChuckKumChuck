9a8de308edca6397ff662877b59d151155d40eaa
dbfjru <dbfjru@gmail.com>
2024-03-15 10:48:56 +0900

    :sparkles: QueryDSL JPA Config 설정

--- src/main/java/com/example/sparta/global/config/JpaConfig.java
+++ src/main/java/com/example/sparta/global/config/JpaConfig.java
@@ -1,5 +1,9 @@
 package com.example.sparta.global.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -7,4 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class JpaConfig {
     // 메인 클래스에서 @EnableJpaAuditing 을 빼기 위한 Config 클래스
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory(){
+    return new JPAQueryFactory(entityManager);
+  }
 }
